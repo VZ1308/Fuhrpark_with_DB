fcfd33e53917cdf3b7d27e8bc32e4161a40759c9
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-09-11 09:04:24 +0200

    Update Fahrrad.java

--- src/Fahrrad.java
+++ src/Fahrrad.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Fahrrad {
+public class Fahrrad extends Fahrzeug {
+    private boolean hatGepaecktraeger;
+
+    public Fahrrad(String marke, String modell, int baujahr, int kilometerstand, boolean hatGepaecktraeger) {
+        super(marke, modell, baujahr, kilometerstand);
+        this.hatGepaecktraeger = hatGepaecktraeger;
+    }
+
+    @Override
+    public String getTableName() {
+        return "Fahrrad";
+    }
+
+    @Override
+    public String getInsertSQL() {
+        return "INSERT INTO Fahrzeug (marke, modell, baujahr, kilometerstand) VALUES ('" +
+                getMarke() + "', '" + getModell() + "', " + getBaujahr() + ", " + getKilometerstand() + ")";
+    }
+
+    @Override
+    public String getInsertSpecificSQL(int id) {
+        return "INSERT INTO Fahrrad (id, marke, modell, baujahr, kilometerstand, hatGepaecktraeger) VALUES (" +
+                id + ", '" + getMarke() + "', '" + getModell() + "', " + getBaujahr() + ", " + getKilometerstand() + ", " + hatGepaecktraeger + ")";
+    }
+
+    @Override
+    public void anzeigen() {
+        System.out.println("Typ: Fahrrad, Marke: " + getMarke() +
+                ", Modell: " + getModell() +
+                ", Baujahr: " + getBaujahr() +
+                ", Kilometerstand: " + getKilometerstand() +
+                ", Gepäckträger: " + hatGepaecktraeger);
+    }
 }
